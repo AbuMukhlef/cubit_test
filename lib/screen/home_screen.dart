83d536cbe43b6b773efdb74b30d120b7562535a5
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-10-07 20:09:17 +0300

    Update home_screen.dart

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,14 +5,24 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Center(
-          child: TextField(
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Search',
-            ),
+          child: Column(
+            children: [
+              const Text('Cubit Screen'),
+              const SizedBox(height: 20),
+              const Text(''),
+              const SizedBox(height: 250),
+              TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Search',
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(onPressed: () {}, child: const Text('Search')),
+            ],
           ),
         ),
       ),
