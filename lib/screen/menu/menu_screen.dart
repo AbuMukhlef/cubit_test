49a858a30779a0c2c91f725e562e708c7596efeb
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-10-11 14:21:27 +0300

    Update menu_screen.dart

--- lib/screen/menu/menu_screen.dart
+++ lib/screen/menu/menu_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cubit_test/core/theme/extensions/theme_ext.dart';
 import 'package:cubit_test/screen/start/widget/box_dec.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
@@ -8,22 +9,39 @@ class MenuScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      decoration: boxDec(),
-      child: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Search',
-                prefixIcon: Icon(Icons.search),
-              ),
-            )
-          ],
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(70),
+          child: Container(
+            decoration: boxDec(),
+            child: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              actions: [
+                CircleAvatar(
+                  radius: 8.w,
+                  child: const Icon(Icons.person),
+                )
+              ],
+            ),
+          ),
         ),
-      ),
-    ));
+        drawer: const Drawer(),
+        body: Container(
+          decoration: boxDec(),
+          child: const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Search',
+                    prefixIcon: Icon(Icons.search),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
