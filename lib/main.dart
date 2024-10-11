a757f0c73e4b933b67eb44752ba391106283fa67
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-10-11 08:05:40 +0300

    downgrading flutter_lints

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:cubit_test/screen/home_screen.dart';
+import 'package:sizer/sizer.dart';
 // import 'package:flutter_dotenv/flutter_dotenv.dart';
 // import 'core/supabase/set_initialize.dart';
 
@@ -12,8 +13,7 @@ Future<void> main() async {
   runApp(
     EasyLocalization(
         supportedLocales: [Locale('en'), Locale('ar')],
-        path:
-            'assets/locales', 
+        path: 'assets/locales',
         fallbackLocale: Locale('en', 'US'),
         child: MainApp()),
   );
@@ -24,12 +24,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      home: HomeScreen(),
+    return Sizer(
+      builder: (context, orientation, screenType) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          home: HomeScreen(),
+        );
+      },
     );
   }
 }
