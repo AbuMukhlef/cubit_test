35101fbc1a6bde522f2e44ba63cfb7b7cc6af81c
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-10-23 14:58:03 +0300

    amer

--- lib/file/read.dart
+++ lib/file/read.dart
@@ -7,35 +7,50 @@ Future<dynamic> importExcel() async {
     type: FileType.custom,
     allowedExtensions: ['xlsx'],
   );
+
+  List<String> names = [];
+  List<String> emails = [];
+
   late dynamic numFormat;
   if (result != null) {
     var bytes = File(result.files.single.path!).readAsBytesSync();
     var excel = Excel.decodeBytes(bytes);
     for (var table in excel.tables.keys) {
-      print(table); //sheet Name
-      print(excel.tables[table]?.maxColumns);
-      print(excel.tables[table]?.maxRows);
+      // print(table); //sheet Name
+      // print(excel.tables[table]?.maxColumns);
+      // print(excel.tables[table]?.maxRows);
       for (var row in excel.tables[table]?.rows ?? []) {
         for (var cell in row) {
-          print('cell ${cell?.rowIndex}/${cell?.columnIndex}');
+          // print('cell ${cell?.rowIndex}/${cell?.columnIndex}');
           final value = cell?.value;
-           numFormat =
-              cell?.cellStyle?.numberFormat ?? NumFormat.standard_0;
-          switch (value) {
-            case null:
-              print('  empty cell');
-              print('  format: $numFormat');
-            case TextCellValue():
-              print('  text: ${value.value}');
-            case FormulaCellValue():
-            case IntCellValue():
-            case BoolCellValue():
-            case DoubleCellValue():
-            case DateCellValue():
-            case TimeCellValue():
-            case DateTimeCellValue():
+          // print('  value: $value');
+          numFormat = cell?.cellStyle?.numberFormat ?? NumFormat.standard_0;
+          if (cell?.rowIndex != 0) {
+            switch (value) {
+              case null:
+                print('  empty cell');
+                print('  format: $numFormat');
+              case TextCellValue():
+                if (cell?.columnIndex == 0) {
+                  names.add('$value.value');
+                } else {
+                  emails.add('$value.value');
+                }
+
+              case FormulaCellValue():
+              case IntCellValue():
+              case BoolCellValue():
+              case DoubleCellValue():
+              case DateCellValue():
+              case TimeCellValue():
+              case DateTimeCellValue():
+            }
           }
         }
+      }
+
+      for (var email in emails) {
+        print(email);
       }
     }
     return numFormat;
