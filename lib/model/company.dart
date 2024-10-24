aedd305e1d604060b829fcfdcc1399ce74b76c80
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-10-24 14:51:15 +0300

    Update company.dart

--- lib/model/company.dart
+++ lib/model/company.dart
@@ -1,11 +1,13 @@
 class Company {
+  final String id;
   final String name;
   final String email;
 
-  Company({required this.name, required this.email});
+  Company({required this.name, required this.email, required this.id});
 
   factory Company.fromJson(Map<String, dynamic> json) {
     return Company(
+      id: json['id'],
       name: json['name'],
       email: json['email'],
     );
@@ -13,6 +15,7 @@ class Company {
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'name': name,
       'email': email,
     };
